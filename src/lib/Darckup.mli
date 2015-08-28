baaa455b3b3d88cc4f6182de296779b868ffee09
Sylvain Le Gall <sylvain@le-gall.net>
2015-08-28 23:13:55 +0200

    Write documentation for Archive/ArchiveSet.

--- src/lib/Darckup.mli
+++ src/lib/Darckup.mli
@@ -6,8 +6,8 @@
 type filename = string
 
 
-(** Archive module provides types and functions to manipulate full backup
-    archives and incremental backup archives. It only acts on naming convention
+(** Archive module provides types and functions to manipulate full and
+  * incremental backup archive filename. It only acts on naming convention
     and doesn't actually read the content of the file,
   *)
 module Archive:
@@ -19,28 +19,43 @@ sig
   val to_filenames: t -> filename list
 end
 
+(** ArchiveSet module provides types and functions to manipulate a set of full
+    and incremental backup archive filenames. It only acts on naming convention
+    and doesn't actually read the content of the file,
+  *)
 module ArchiveSet:
 sig
   type t
 
-  (** [of_filenames] returns the archive representation of the filenames and
+  (** [of_filenames lst] returns the archive representation of the filenames and
       the list of rejected filenames.
     *)
   val of_filenames: filename list -> t * filename list
 
+  (** [to filenames t] returns the sorted list of filename in this archive set.
+    *)
   val to_filenames: t -> filename list
 
-  (** [lenght] returns the number of archive. *)
+  (** [lenght] returns the number of archive.
+    *)
   val length: t -> int
 
+  (** [last t] returns the last existing archive.
+    *)
   val last: t -> Archive.t
 
-  (* [next t max_incremental prefix_full] returns either the latest prefix with
-     an incremented suffix or [prefix_full ^ "full"].
-   *)
+  (** [next t max_incremental prefix_full] returns either the latest prefix
+      with an incremented suffix or [prefix_full ^ "full"].
+    *)
   val next: t -> int -> filename -> filename
 
+  (** [pop t] returns the archive set without the first element, which is
+      returned as well.
+    *)
   val pop: t -> t * Archive.t
 
+  (** [npop n t] returns the first [n] archives from the set and the updated
+      archive set.
+    *)
   val npop: int -> t -> t * Archive.t list
 end
